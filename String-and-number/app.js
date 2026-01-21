ac87b617a61dec2aab0581804f09ca34243248d6
muhammadabdullah-dev1 <muhammadabdullah53897@gmail.com>
2026-01-21 16:16:06 +0500

    6 quetion done string

---
+++ String-and-number/app.js
@@ -0,0 +1,48 @@
+
+// let str = "Hello World, welcome to the world of JavaScript";
+
+// console.log(str.toLowerCase());
+// console.log(str.toUpperCase());
+
+//  function toLowerCase(name) {
+//     return name.toLowerCase();
+    
+// }
+// console.log(toLowerCase("ABDuLLAh"));
+
+
+
+
+
+
+
+// let text = "hello"
+// console.log(text.toUpperCase());
+
+
+
+
+// function count(str){
+//     return str.length;
+// }
+// console.log(count("hello"));
+
+
+
+// let fruit="apple";
+// console.log(fruit.indexOf("apple"));]
+
+
+
+
+
+// let sentence = "hey i am a webdeveloper"
+// let spaceIndex= sentence.indexOf(" ");
+// // console.log(sentence.indexOf(" "));
+// console.log(sentence.slice(0,spaceIndex));
+
+
+
+
+
+let replaceStr="Babar azam is a good batsman. Babar azam plays well. He is a captain of Pakistan cricket team. Babar azam is a right handed batsman. Babar azam is a top order batsman. Babar azam is a best batsman."
