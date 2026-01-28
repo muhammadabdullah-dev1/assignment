f1f56d959bfd5a4125fb2752d6c3cd315a1ff528
muhammadabdullah-dev1 <muhammadabdullah53897@gmail.com>
2026-01-28 15:26:19 +0500

    10 quetions done

--- String-and-number/app.js
+++ String-and-number/app.js
@@ -30,7 +30,7 @@
 
 
 // let fruit="apple";
-// console.log(fruit.indexOf("apple"));]
+// console.log(fruit.indexOf("apple"));
 
 
 
@@ -45,4 +45,85 @@
 
 
 
-let replaceStr="Babar azam is a good batsman. Babar azam plays well. He is a captain of Pakistan cricket team. Babar azam is a right handed batsman. Babar azam is a top order batsman. Babar azam is a best batsman."
+// let replaceStr="Babar azam is a good batsman. Babar azam plays well. He is a captain of Pakistan cricket team. Babar azam is a right handed batsman. Babar azam is a top order batsman. Babar azam is a best batsman."
+// console.log(replaceStr.replaceAll("Babar azam","Rizwans"));
+
+
+
+
+
+
+// let replaceStr="Bad"
+// console.log(replaceStr.replace("Bad" , "Good"));
+
+
+
+
+
+
+
+//  function replaceVowel(perameters) {
+//     return perameters.replaceAll(/[aeiou]/gi,"*")
+// }
+
+// console.log(replaceVowel("hey i am a webdeveloper"));
+
+
+
+// let sentence="hey! i am a developer";
+// console.log(sentence.split(" "));
+
+
+
+
+
+
+
+
+// let word2="abdullah";
+// console.log(word2[0].toUpperCase()+word2.slice(1));
+
+
+
+
+// function sentence1(word) {
+//     let sentences= word.split(" ");
+//     return sentences[1];
+    
+    
+// }
+// console.log(sentence1("hey its me"));
+
+
+
+
+
+
+
+// let sentence= " many dumb boys in the dumb class i have not seen that much dumb guys in my intire life "
+// console.log(sentence.replaceAll("dumb","****"));
+
+
+
+// let sentence ="many dumb boys ";
+// console.log((sentence.split(" ").length));
+
+
+
+
+
+
+// console.log (sentence.indexOf("cat"));
+function sentence(para) {
+    let index=para.indexOf("cat")
+if (index !==-1) {
+    return true;
+    
+}else{
+    return false;
+
+    
+}
+}
+console.log(sentence("i have many cat cat cat"));
+
