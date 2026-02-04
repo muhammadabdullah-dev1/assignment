7f80395b8ba7cf0bb0f59288a7b1c823dde03566
muhammadabdullah-dev1 <muhammadabdullah53897@gmail.com>
2026-02-04 15:01:10 +0500

    functions folder added

---
+++ Date-&-time/app.js
@@ -0,0 +1,14 @@
+// let currentDate= new Date()
+// console.log(currentDate);
+// // console.log(typeof currentDate);
+// console.log(currentDate.toString());
+
+
+
+
+
+function frormattedDate(date){
+return date.getDate(),date.getMonth();
+
+}
+console.log(frormattedDate(date));
---
+++ Functions/functions.js
@@ -0,0 +1,18 @@
+// console.log("hello world");
+// function sayHello() {
+//     console.log("Hello");
+    
+// }
+// sayHello()
+
+
+
+
+
+function printName(name) {
+   return "My name is "+ name;
+    
+    
+}
+console.log(printName("Scar"));
+ 
