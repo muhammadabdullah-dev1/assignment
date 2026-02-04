a5b18be6d32eef184b94529673b1f031ee48d4e9
muhammadabdullah-dev1 <muhammadabdullah53897@gmail.com>
2026-02-04 15:48:55 +0500

    some fun quetions done

--- Functions/functions.js
+++ Functions/functions.js
@@ -9,10 +9,43 @@
 
 
 
-function printName(name) {
-   return "My name is "+ name;
+// function printName(name) {
+//    return "My name is "+ name;
     
     
+// }
+// console.log(printName("Scar"));
+ 
+
+
+// function sumOfNumber(num1, num2) {
+//     console.log(  num1 + num2);
+   
+// }
+// sumOfNumber(5,5)
+
+
+
+
+
+// function takeingSquare(num) {
+//     return Math.pow(num,2)
+// }
+// console.log( takeingSquare(10));
+
+
+
+
+function EvenOdd(num) {
+    if (num%2 === 0) {
+        return True;
+        
+    }else{
+        return false;
+        
+    }
+   
 }
-console.log(printName("Scar"));
+console.log(EvenOdd(9));
+
  
