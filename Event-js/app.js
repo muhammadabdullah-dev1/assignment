342c541b61e80159c27b925149e960d5c2c3bf0c
muhammadabdullah-dev1 <muhammadabdullah53897@gmail.com>
2026-03-07 00:54:16 +0500

    event is add

---
+++ Event-js/app.js
@@ -0,0 +1,23 @@
+// let button = document.getElementById("btn")
+// let bColor = document.getElementById("container")
+
+// button.addEventListener("click", () => {
+//     // document.querySelector("container").innerHTML =
+//     bColor.style.backgroundColor = "red";
+// })
+
+
+
+
+// let button = document.getElementById("btn")
+// let bColor = document.getElementById("container")
+// button.addEventListener("click", () => {
+//     bColor.style.backgroundColor = "black"
+// })
+
+
+let button = document.getElementById("btn")
+let text = document.getElementById("text")
+button.addEventListener("click", () => {
+    text.style.display = "none"
+})
